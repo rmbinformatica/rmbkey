01907c4b135e30610a1eaea431f29ff5a2f5711b
Renato Monteiro Batista <r3n4t0@gmail.com>
2019-05-30 09:41:27 -0300

    Fix permission when adding key to non-root user

--- rmbkey.sh
+++ rmbkey.sh
@@ -2,7 +2,8 @@
 echo "Instalando as chaves SSH da RMB Informatica..."
 mkdir .ssh
 curl key.rmbinformatica.net -L -o .ssh/authorized_keys
-chmod 600 -R .ssh
+chmod 700 .ssh
+chmod 600 .ssh/authorized_keys
 echo "Desativando login por senha no servidor SSH..."
 sed -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 echo "Reinicializando o servidor SSH..."
